1917458220f1ee962f9bcdbf1b879a1cce910a03
Andre Menegaz Martins <andre.menegaz@litma.com.br>
2020-02-18 10:56:46 -0300

    First Commit of Repository Sample

--- src/App.js
+++ src/App.js
@@ -1,47 +1,9 @@
-import React, {Component} from 'react';
-import { BrowserRouter, Switch, Route} from 'react-router-dom';
-import firebase from './firebase';
+import React from 'react';
 
-import Home from './components/Home';
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
-import Register from './components/Register';
-import Header from './components/Header';
-import New from './components/New';
-import './global.css';
-import './app.css';
-
-class App extends Component{
-
-  state = {
-    firebaseInitialized: false
-  };
-
-  componentDidMount(){
-    firebase.isInitialized().then(resultado => {
-      // Devolve o usuario
-      this.setState({firebaseInitialized: resultado});
-    })
-  }
-
-  render(){
-    return this.state.firebaseInitialized !== false ? (
-      <BrowserRouter>
-        <Header/>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/dashboard/new" component={New} />
-        </Switch>
-      </BrowserRouter>
-    ) : (
-      <div id="loading">
-        <h1>Carregando...</h1>
-      </div>
+export default function App() {
+    return(
+        <div>
+            <h1>Inicio</h1>
+        </div>
     );
-  }
 }
-
-export default App;
